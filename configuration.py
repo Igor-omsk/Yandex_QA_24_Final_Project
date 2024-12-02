7bbb97f61e8c0306e05f574f0ce556f93e2c769f
Igor_omsk <igor_omsk@mail.ru>
2024-12-02 17:44:09 +0600

    Написал комментарии

--- configuration.py
+++ configuration.py
@@ -2,8 +2,8 @@
 # Значение должно быть скопировано из настроек или документации сервиса, к которому предоставляется доступ.
 URL_SERVICE = "https://4aeabe35-6c9a-403d-a753-d20532bc4f6b.serverhub.praktikum-services.ru"
 
-# CREATE_ORDER хранит путь к API-методу для создания нового заказа
+# CREATE_ORDER хранит путь к API-методу для создания нового заказа.
 CREATE_ORDER = "/api/v1/orders"
 
-# GET_ORDER_INFO хранит путь к API-методу для получения заказа по его номеру
+# GET_ORDER_INFO хранит путь к API-методу для получения заказа по его номеру.
 GET_ORDER_INFO = "/api/v1/orders/track"
